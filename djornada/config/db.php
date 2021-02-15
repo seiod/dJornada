30b84a6100b54046f80573bc6bbe7b54b8204876
Jose <77276470+seiod@users.noreply.github.com>
2021-02-15 14:30:56 +0100

    Update db.php

--- djornada/config/db.php
+++ djornada/config/db.php
@@ -1,7 +1,7 @@
 <?php
 class Database{
     public static function connect(){
-        $db = new mysqli('localhost', 'id16044868_administrador', 'kym-aa3W]ufL<A]a', 'id16044868_djornada_db');
+        $db = new mysqli('localhost', '', '', '');
         $db->query("SET NAMES 'UTF8'");
         return $db;
     }
